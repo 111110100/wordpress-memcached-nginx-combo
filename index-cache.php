b821ea6d4b3ab9a8db1e90e193db287b7aa23ebb
111110100 <elomibao@gmail.com>
2019-12-02 12:00:52 +0800

    Create index-cache.php

---
+++ index-cache.php
@@ -0,0 +1,35 @@
+<?php
+$debug = True;
+
+$start = microtime(true);
+
+$memcached = new Memcached();
+$memcached->addServer('127.0.0.1', 11211);
+#use line below if memcached is running as socket
+#$memcached->addServer('/var/run/memcached.sock', 0);
+
+$cacheKey = "fullpage:{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+$cacheTime = $_SERVER['REQUEST_URI'] == '/' ? 60 : 300; # homepage 1min, 5min the rest. Adjust to your own liking.
+
+$debugMessage = 'Page retrieved from cache in %f seconds.';
+$html = $memcached->get($cacheKey);
+
+if (! $html) {
+  $debugMessage = 'Page generated in %f seconds.';
+
+  ob_start();
+
+  require 'index.php';
+  $html = ob_get_contents();
+
+  $memcached->set($cacheKey, $html, $cacheTime);
+
+  ob_end_clean();
+}
+
+$finish = microtime(true);
+
+echo $html;
+if ($debug) { echo '<-- ' . sprintf($debugMessage, $finish - $start) . ' Cached for ' . strval($cacheTime) . ' seconds -->'; }
+exit;
+?>
