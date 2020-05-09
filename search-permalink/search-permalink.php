e734b890bcf349a06233f4e5f2b75537e5e147d5
Erwin Lomibao <erwin.lomibao@@inquirer.net>
2020-05-10 00:05:16 +0800

    Upaded function name

--- search-permalink/search-permalink.php
+++ search-permalink/search-permalink.php
@@ -15,12 +15,12 @@
 
 defined( 'ABSPATH' ) || exit;
 
-function wpb_change_search_url() {
+function change_search_url() {
     if ( is_search() && ! empty( $_GET['s'] ) ) {
         wp_redirect( home_url( "/search/" ) . urlencode( get_query_var( 's' ) ) );
         exit();
     }   
 }
-add_action( 'template_redirect', 'wpb_change_search_url' );
+add_action( 'template_redirect', 'change_search_url' );
 
 ?>
