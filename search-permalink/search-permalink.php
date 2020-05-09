58e0e106c49fd1431bec03e539e712e2d2e4c5a0
Erwin Lomibao <erwin.lomibao@@inquirer.net>
2020-05-09 21:21:42 +0800

    Added search-permalink.php plugin

---
+++ search-permalink/search-permalink.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package search-permalink 
+ * @author erwin lomibao 
+ * @version 1.0 
+ * @license commercial
+ * 
+ * @wordpress-plugin
+ * Plugin Name: Search Permalink 
+ * Description: Makes search results SEO friendly and cacheable. Accompanying plugin for https://github.com/111110100/wordpress-memcached-nginx-combo
+ * Version: 1.0
+ * Author: Erwin Lomibao
+ * Author URI: https://github.com/111110100/wordpress-memcached-nginx-combo
+ */
+
+function wpb_change_search_url() {
+    if ( is_search() && ! empty( $_GET['s'] ) ) {
+        wp_redirect( home_url( "/search/" ) . urlencode( get_query_var( 's' ) ) );
+        exit();
+    }   
+}
+add_action( 'template_redirect', 'wpb_change_search_url' );
+
+?>
